8187fb78047b8f5d3d50960b477dacd9dc55a507
larkenx <larkenx@gmail.com>
2019-02-01 09:12:24 -0500

    working on shading

--- simple-raytracing.go
+++ simple-raytracing.go
@@ -5,6 +5,7 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math"
 	"os"
 )
 
@@ -42,6 +43,34 @@ func getRayColorRatio(r Ray) Vector3D {
 	return Vector3D{1, 1, 1}.Mul(1 - t).Add(Vector3D{0.5, 0.7, 1}.Mul(t))
 }
 
+func sub(p float64, q float64) float64 {
+	return p - q
+}
+
+func hitsSphere(center Vector3D, radius float64, r Ray) float64 {
+	oc := r.origin.Sub(center)
+	a := r.direction.Dot(r.direction)
+	b := float64(oc.Dot(r.direction) * 2)
+	c := float64(oc.Dot(oc) - radius*radius)
+	discriminant := float64(b*b - 4*a*c)
+	if discriminant < 0 {
+		return -1.0
+	}
+	return (-b - math.Sqrt(discriminant)) / (2.0 * a)
+}
+
+func colorSphere(r Ray) Vector3D {
+	center := Vector3D{0, 0, -1}
+	t := hitsSphere(center, 0.5, r)
+	if t > 0.0 {
+		n := r.PositionAt(t).Sub(Vector3D{0, 0, -1}).Unit()
+		return Vector3D{n.x + 1, n.y + 1, n.z + 1}.Mul(0.5)
+	}
+	unitDirection := r.direction.Unit()
+	t = 0.5 * (unitDirection.y + 1.0)
+	return Vector3D{1, 1, 1}.Mul(1 - t).Add(Vector3D{0.5, 0.7, 1}.Mul(t))
+}
+
 func main() {
 	// Create a blank image 100x200 pixels
 	width := 200
@@ -59,10 +88,12 @@ func main() {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			u := float64(x) / float64(width)
 			v := float64(y) / float64(height)
-			rayColor := getRayColorRatio(Ray{origin, lowerLeftCorner.Add(horizontal.Mul(u), vertical.Mul(v))})
-			r := uint8(255 * rayColor.x)
-			g := uint8(255 * rayColor.y)
-			b := uint8(255 * rayColor.z)
+			ray := Ray{origin: origin, direction: lowerLeftCorner.Add(horizontal.Mul(u), vertical.Mul(v))}
+			// colorRatios := getRayColorRatio(ray)
+			colorRatios := colorSphere(ray)
+			r := uint8(255 * colorRatios.x)
+			g := uint8(255 * colorRatios.y)
+			b := uint8(255 * colorRatios.z)
 			img.SetRGBA(x, y, color.RGBA{r, g, b, 255})
 		}
 	}
